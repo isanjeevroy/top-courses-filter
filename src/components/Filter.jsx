1879f6b5b8ed4e4772d5bd49dbef244fc1e86f1d
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-05-28 10:11:31 +0530

    UI updated

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Filter = (props) => {
     let filterData = props.filterData;
+    let category = props.category;
     let setCategory = props.setCategory;
     function filterHandler(title){
         setCategory(title);
@@ -11,9 +12,13 @@ const Filter = (props) => {
         <div>
             {
                 filterData.map((data) => {
-                    return (<button className='mx-2 px-2 rounded-sm bg-slate-600' key={data.id}
+                    return (
+                    <button className={`mx-2 px-2 rounded-sm bg-slate-600 
+                    ${category===data.title? "border-2 border-red-500" : ""}`} 
+                    key={data.id}
                     onClick={()=>filterHandler(data.title)}
-                    >{data.title}</button>)
+                    >{data.title}
+                    </button>)
                 })
             }
         </div>
